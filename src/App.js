771aecab8445de404a224fc26ce279c27f52bc8b
root <root@DESKTOP-1ML003O.localdomain>
2019-11-18 11:00:33 +1100

    added try cathc to app.js

--- src/App.js
+++ src/App.js
@@ -14,19 +14,32 @@ class App extends React.Component {
   }
   
   getUser = async () => {
-    const { setCurrentUser } = this.props;
-    const response = await axios.get('https://cobalt-shop.herokuapp.com/user')
-    const user = response.data
-    if (user.loggedIn === true) {
-      setCurrentUser(user)
+
+    try {
+      const { setCurrentUser } = this.props;
+      const response = await axios.get('https://cobalt-shop.herokuapp.com/user')
+      const user = response.data
+      console.log(user)
+      if (user.loggedIn === true) {
+        setCurrentUser(user)
+      }
+    } catch (error) {
+      console.log(error)
     }
+    
   }
 
   signOut = async () => {
-    const { setCurrentUser } = this.props;
-    const response = await axios.get('https://cobalt-shop.herokuapp.com/logOut') 
-    setCurrentUser(null)
-    return response.data.loggedOut
+
+    try {
+      const { setCurrentUser } = this.props;
+      const response = await axios.get('https://cobalt-shop.herokuapp.com/logOut') 
+      setCurrentUser(null)
+      return response.data.loggedOut 
+    } catch (error) {
+       console.log(error) 
+    }
+    
   }
 
   render() {
